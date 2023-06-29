09fd8dcc0e99c7a490773a34df30ae9550025dda
suman singh <sumansingh.dev7870@gmail.com>
2023-06-29 18:36:08 +0530

    first commit

---
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'npst-recipe-details',
+  templateUrl: './recipe-details.component.html',
+  styleUrls: ['./recipe-details.component.css']
+})
+export class RecopeDetailsComponent {
+
+}
