ab308ddbaf86545371e975b95f06d1ff90a8bbfa
suman <suman@active.agency>
2023-07-06 00:46:19 +0530

    Navigation added by if else

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'first-project';
+  shoping: boolean = false;
+
+  onChangeHeader(header: string) {
+    console.log(header, 'PPPPPPPPPPP');
+    if (header === 'shoping') {
+      this.shoping = true;
+    }
+
+    if (header === 'recipe') {
+      this.shoping = false;
+    }
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,20 @@
-import { Component} from '@angular/core'
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-    selector: 'npst-header',
-    templateUrl: './header.component.html'
+  selector: 'npst-header',
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-    collapsed=true
+  collapsed = true;
+
+  @Output() target = new EventEmitter<string>();
+
+  onRecipe() {
+    console.log('recipe 1111111');
+    this.target.emit('recipe');
+  }
+  onShoping() {
+    console.log('Shoping 2222222');
+    this.target.emit('shoping');
+  }
 }
